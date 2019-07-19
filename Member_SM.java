d389e6910c51f755f3309504b8d914d5e280bb11
Lee <seoungmin104@naver.com>
2019-07-19 15:48:35 +0900

    회원, 가게 변수목록_1차

---
+++ Member_SM.java
@@ -0,0 +1,35 @@
+package map_for_lunch;
+
+public class Member {
+
+	private String memberId;
+	private String memberPass;
+	private String memberName;
+	private String memberBirth;
+	
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getMemberPass() {
+		return memberPass;
+	}
+	public void setMemberPass(String memberPass) {
+		this.memberPass = memberPass;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public String getMemberBirth() {
+		return memberBirth;
+	}
+	public void setMemberBirth(String memberBirth) {
+		this.memberBirth = memberBirth;
+	}
+	
+}
---
+++ Store_SM.java
@@ -0,0 +1,38 @@
+package map_for_lunch;
+
+public class Store {
+
+	private int storePrice;
+	private String storeName;
+	private String storeMenu;
+	private String storePlace;
+	
+	public int getStorePrice() {
+		return storePrice;
+	}
+	public void setStorePrice(int storePrice) {
+		this.storePrice = storePrice;
+	}
+	public String getStoreName() {
+		return storeName;
+	}
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
+	public String getStoreMenu() {
+		return storeMenu;
+	}
+	public void setStoreMenu(String storeMenu) {
+		this.storeMenu = storeMenu;
+	}
+	public String getStorePlace() {
+		return storePlace;
+	}
+	public void setStorePlace(String storePlace) {
+		this.storePlace = storePlace;
+	}
+	
+	
+	
+	
+}
