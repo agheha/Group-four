45fa24bf304f8777dbcae3846d25b58b8cfd4694
Lee <seoungmin104@naver.com>
2019-07-24 18:11:11 +0900

    store 변수 목록

--- Store_SM.java
+++ Store_SM.java
@@ -2,11 +2,44 @@ package map_for_lunch;
 
 public class Store {
 
+	private int storeNo;
 	private int storePrice;
 	private String storeName;
 	private String storeMenu;
 	private String storePlace;
+	private String storeType;
+	private int storeGrade;
+	private String review;
 	
+	
+	
+	
+	
+	public int getStoreGrade() {
+		return storeGrade;
+	}
+	public void setStoreGrade(int storeGrade) {
+		this.storeGrade = storeGrade;
+	}
+	public String getReview() {
+		return review;
+	}
+	public void setReview(String review) {
+		this.review = review;
+	}
+	public int getStoreNo() {
+		return storeNo;
+	}
+	public void setStoreNo(int storeNo) {
+		this.storeNo = storeNo;
+	}
+	
+	public String getStoreType() {
+		return storeType;
+	}
+	public void setStoreType(String storeType) {
+		this.storeType = storeType;
+	}
 	public int getStorePrice() {
 		return storePrice;
 	}
