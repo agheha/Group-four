93cdc5045cd7e3adae2e2a578d5c57f0e9f7970e
agheha <youbeen0712@gmail.com>
2019-07-25 20:31:26 +0900

    Member class를 기반으로 회원관리 메뉴(MemberUI) 및 login page 초안 작성

---
+++ Member/BaseUI_YB.java
@@ -0,0 +1,29 @@
+package map_for_lunch;
+
+import java.util.Scanner;
+
+public abstract class BaseUI {
+	protected Scanner sc = new Scanner(System.in);
+	
+	// 모든 상속받는 UI 클래스가 구현할 공통의 메서드 정의
+	public abstract void service();
+		// 개념상 강제화 해서 자식이 반드시 받을수있도록 해준다.
+	
+	
+	
+	// String title = getString("제목을 입력하세요 : ");
+	protected String getString(String msg) {
+		System.out.println(msg);
+		return sc.nextLine();
+	}
+	protected int getInt(String msg) {
+		/*
+		System.out.println(msg);
+		return Integer.parseInt(sc.nextLine());
+		*/
+		return Integer.parseInt(getString(msg));
+		
+	}
+	
+	
+}
