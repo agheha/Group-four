b624ccb61cc849bc875131383c695020413d0eec
ddaadd90 <paprbleq@gmail.com>
2019-08-19 20:09:36 +0900

    회원로그인 - 가게보기 UI

---
+++ store/list/ui/priceUI2.java
@@ -0,0 +1,59 @@
+package store.list.ui;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+import login.vo.User;
+import member.dao.MemberDAO;
+import ui.BaseUI;
+import vo.Store;
+
+public class priceUI2 extends BaseUI {
+	private Scanner sc = new Scanner(System.in);
+	private MemberDAO dao;
+
+	public priceUI2(MemberDAO dao) {
+		this.dao = dao;
+	}
+
+	public void service() {
+		List<Store> list = dao.selectMember();
+		System.out.println("-------------------------------");
+		System.out.println("가격순으로 보기");
+		System.out.println("-------------------------------");
+		System.out.println("가게번호    가게이름     메인메뉴    가격");
+		System.out.println("-------------------------------");
+		int index = 1;
+		Map<Integer, Store> priceStore = new TreeMap<>(); 
+		for (Store b : list) {
+			priceStore.put(index, b);
+			System.out.printf("%3d %10s %10s %10d원\n", 
+					index++, 
+					b.getStoName(), 
+					b.getRstMenu(),
+					b.getRstPrice());
+		}
+		System.out.println("-------------------------------");
+		System.out.println("상세보기((상단의)가게번호를 선택해주세요)");
+		System.out.println("이전으로 돌아가기 (0번을 선택해주세요)");
+		System.out.println("-------------------------------");
+		System.out.print("번호를 입력하세요 : ");
+		int no = Integer.parseInt(sc.nextLine());
+
+		System.out.println("-------------------------------");
+		System.out.println("상세보기");
+		System.out.println("-------------------------------");
+		System.out.println("가게번호    가게이름    메인메뉴    가격");
+		System.out.println("-------------------------------");
+		
+		// 메뉴테이블에서 나머지 메뉴받아와야함
+		Store store = priceStore.get(no);
+		System.out.print("    " + store.getStoNo());
+		System.out.print("    " + store.getStoName());
+		System.out.print("    " + store.getRstMenu());
+		System.out.println("    " + store.getRstPrice());
+
+	}
+}
